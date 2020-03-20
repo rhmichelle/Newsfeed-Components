b8697b04649f9c530707bff39a881f640585c04c
Michelle Rodriguez Harrison <dev@michellerh.com>
2020-03-19 21:34:20 -0400

    Added article.less and started component

--- Article/Article.js
+++ Article/Article.js
@@ -112,3 +112,12 @@ const data = [
   Step 5: Add a new article to the array. Make sure it is in the same format as the others. Refresh the page to see the new article.
 
 */
+
+const newsfeedComponent = () => {
+  const title = document.createElement('h2')
+  const date = document.createElement('p')
+  const p1 = document.createElement('p')
+  const p2 = document.createElement('p')
+  const p3 = document.createElement('p')
+  const expandButton = document.createElement('span')
+}
